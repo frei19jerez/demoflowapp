cb9c38d7b69ca39e5c02059cc811b8c8d2830376
Freimel Jerez <frei19jerez@gmail.com>
2026-04-18 14:50:32 -0500

    actualiza todo

--- config/datastores.js
+++ config/datastores.js
@@ -2,13 +2,7 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-postgresql',
-
-    // Producción (Render / nube)
-    url: process.env.DATABASE_URL ||
-
-      // Local (tu PC)
-      'postgresql://postgres:emily19kenia@localhost:5432/demoflow',
-
+    url: process.env.DATABASE_URL || 'postgresql://postgres:emily19kenia@localhost:5432/demoflow',
     ssl: process.env.DATABASE_URL
       ? { rejectUnauthorized: false }
       : false
