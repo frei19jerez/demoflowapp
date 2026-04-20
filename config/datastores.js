347a3dcc07bb67c5291f922faa2e1cdb11042442
Freimel Jerez <frei19jerez@gmail.com>
2026-04-20 06:16:06 -0500

    mejora datastores para render y local

--- config/datastores.js
+++ config/datastores.js
@@ -2,7 +2,12 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-postgresql',
-    url: process.env.DATABASE_URL
+
+    url: process.env.DATABASE_URL || 'postgresql://postgres:postgres@localhost:5432/demoflow',
+
+    ssl: process.env.DATABASE_URL
+      ? { rejectUnauthorized: false }
+      : false
   }
 
 };
