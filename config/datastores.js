86b2bedd1189f7334814fb9bd99fbdb97ca7f909
Freimel Jerez <frei19jerez@gmail.com>
2026-05-17 08:46:22 -0500

    demoflow limpio

--- config/datastores.js
+++ config/datastores.js
@@ -2,16 +2,11 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-postgresql',
+    url: process.env.DATABASE_URL,
 
-    url: process.env.DATABASE_URL
-      ? process.env.DATABASE_URL
-      : 'postgresql://postgres:emily19kenia@localhost:5432/demoflow',
-
-    ssl: process.env.DATABASE_URL
+    ssl: process.env.NODE_ENV === 'production'
       ? {
-          ssl: {
-            rejectUnauthorized: false
-          }
+          rejectUnauthorized: false
         }
       : false
   }
