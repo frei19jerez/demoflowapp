c07cfd2f710309129f197b3a84fa925d56c5546a
Freimel Jerez <frei19jerez@gmail.com>
2026-05-25 19:56:04 -0500

    corrige modelo iahistorial

--- api/models/IAHistorial.js
+++ api/models/IAHistorial.js
@@ -9,29 +9,54 @@ module.exports = {
 
   attributes: {
 
+    id: {
+      type: 'number',
+      autoIncrement: true,
+      columnName: 'id'
+    },
+
     usuario: {
       model: 'usuario',
-      required: true
+      required: true,
+      columnName: 'usuario'
     },
 
     proyecto: {
       model: 'proyecto',
-      allowNull: true
+      allowNull: true,
+      columnName: 'proyecto'
     },
 
     tipo: {
       type: 'string',
-      defaultsTo: 'dashboard'
+      defaultsTo: 'dashboard',
+      columnName: 'tipo'
     },
 
     resultado: {
       type: 'json',
-      columnType: 'json'
+      columnType: 'jsonb',
+      columnName: 'resultado'
     },
 
     creditosUsados: {
       type: 'number',
-      defaultsTo: 1
+      defaultsTo: 1,
+      columnName: 'creditos_usados'
+    },
+
+    createdAt: {
+      type: 'ref',
+      columnType: 'timestamp',
+      autoCreatedAt: true,
+      columnName: 'created_at'
+    },
+
+    updatedAt: {
+      type: 'ref',
+      columnType: 'timestamp',
+      autoUpdatedAt: true,
+      columnName: 'updated_at'
     }
 
   }
