88e79fa8069fadca4d65234feab7a6250c40c80d
Freimel Jerez <frei19jerez@gmail.com>
2026-06-17 09:33:17 -0500

    Separar cookie de sesion DemoFlow

--- config/session.js
+++ config/session.js
@@ -1,39 +1,18 @@
 /**
  * Session Configuration
  * (sails.config.session)
- *
- * Use the settings below to configure session integration in your app.
- * (for additional recommended settings, see `config/env/production.js`)
- *
- * For all available options, see:
- * https://sailsjs.com/config/session
  */
 
 module.exports.session = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Session secret is automatically generated when your new app is created   *
-  * Replace at your own risk in production-- you will invalidate the cookies *
-  * of your users, forcing them to log in again.                             *
-  *                                                                          *
-  ***************************************************************************/
-  secret: '78e735772b824e99011afb91605e870f',
+  secret: process.env.SESSION_SECRET || '78e735772b824e99011afb91605e870f',
 
+  name: 'demoflow.sid',
 
-  /***************************************************************************
-  *                                                                          *
-  * Customize when built-in session support will be skipped.                 *
-  *                                                                          *
-  * (Useful for performance tuning; particularly to avoid wasting cycles on  *
-  * session management when responding to simple requests for static assets, *
-  * like images or stylesheets.)                                             *
-  *                                                                          *
-  * https://sailsjs.com/config/session                                       *
-  *                                                                          *
-  ***************************************************************************/
-  // isSessionDisabled: function (req){
-  //   return !!req.path.match(req._sails.LOOKS_LIKE_ASSET_RX);
-  // },
+  cookie: {
+    maxAge: 24 * 60 * 60 * 1000,
+    sameSite: 'lax',
+    secure: process.env.NODE_ENV === 'production'
+  }
 
 };
